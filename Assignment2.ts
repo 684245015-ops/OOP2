aba414d6ae3c79c76fd216da0560814a78a3341d
684245015-ops <684245015@webmail.npru.ac.th>
2026-06-25 16:25:26 +0700

    ส่งงานAssignment2

---
+++ Assignment2.ts
@@ -0,0 +1,22 @@
+let students: [string, number][] = [
+    ["Alice", 95],["Bob", 82],["Charlie", 76],["David", 60],["Emma", 45]];
+
+for (let student of students) {
+    let name: string = student[0];
+    let score: number = student[1];
+    let grade: string;
+
+    if (score >= 80) {
+        grade = "A";
+    } else if (score >= 70) {
+        grade = "B";
+    } else if (score >= 60) {
+        grade = "C";
+    } else if (score >= 50) {
+        grade = "D";
+    } else {
+        grade = "F";
+    }
+
+    console.log(`${name} received ${score} and grade ${grade}`);
+}
